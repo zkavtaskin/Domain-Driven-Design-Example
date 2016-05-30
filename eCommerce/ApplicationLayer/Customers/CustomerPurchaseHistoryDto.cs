985dfb229ff357dc66690cac29aa279f2a6cce3d
Zan Kavtaskin <zan.kavtaskin@gmail.com>
2016-05-30 20:18:10 +0100

    Missing file

---
+++ eCommerce/ApplicationLayer/Customers/CustomerPurchaseHistoryDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace eCommerce.ApplicationLayer.Customers
+{
+    public class CustomerPurchaseHistoryDto
+    {
+        public Guid CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int TotalPurchases { get; set; }
+        public int TotalProductsPurchased { get; set; }
+        public decimal TotalCost { get; set; }
+    }
+}
