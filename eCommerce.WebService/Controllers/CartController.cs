d3770c6bde0c6b82761789ea7849a332b17e4f3e
Edonis Seferi <edonis.seferi@asos.com>
2019-02-19 17:09:40 +0000

    Exposed the share feature on the cart service and controller. Refactored the cart controller.

--- eCommerce.WebService/Controllers/CartController.cs
+++ eCommerce.WebService/Controllers/CartController.cs
@@ -1,10 +1,6 @@
 ﻿using eCommerce.WebService.Models;
 using eCommerce.ApplicationLayer.Carts;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace eCommerce.WebService.Controllers
@@ -24,74 +20,41 @@ namespace eCommerce.WebService.Controllers
             this.cartService = cartService;
         }
 
+        [HttpPost]
+        public Response<CartDto> Add(Guid customerId, [FromUri]CartProductDto cartDto) 
+            => CreateResponse(() => this.cartService.Add(customerId, cartDto));
+
         [HttpGet]
-        public Response<CartDto> Add(Guid customerId, [FromUri]CartProductDto cartDto)
+        public Response<CartDto> GetById(Guid customerId) 
+            => CreateResponse(() => this.cartService.Get(customerId));
+
+        [HttpPost]
+        public Response<CartDto> Remove(Guid customerId, Guid productId) 
+            => CreateResponse(() => this.cartService.Remove(customerId, productId));
+
+
+        [HttpPost]
+        public Response<CheckOutResultDto> Checkout(Guid customerId) 
+            => CreateResponse(() => this.cartService.CheckOut(customerId));
+
+        [HttpPost]
+        public Response<CartDto> Share(Guid cartOwnerId, Guid cartRecipientId) 
+            => CreateResponse(() => this.cartService.Share(cartOwnerId, cartRecipientId));
+
+        private Response<T> CreateResponse<T>(Func<T> execute)
         {
-            Response<CartDto> response = new Response<CartDto>();
+            var response = new Response<T>();
             try
             {
-                response.Object = this.cartService.Add(customerId, cartDto);
+                response.Object = execute();
             }
             catch (Exception ex)
             {
-                //log error
                 response.Errored = true;
                 response.ErrorMessage = ex.Message;
             }
+
             return response;
         }
-
-        [HttpGet]
-        public Response<CartDto> GetById(Guid customerId)
-        {
-            Response<CartDto> response = new Response<CartDto>();
-            try
-            {
-                response.Object = this.cartService.Get(customerId);
-            }
-            catch (Exception ex)
-            {
-                //log error
-                response.Errored = true;
-                response.ErrorMessage = ex.Message;
-            }
-            return response;
-        }
-
-        [HttpGet]
-        public Response<CartDto> Remove(Guid customerId, Guid productId)
-        {
-            Response<CartDto> response = new Response<CartDto>();
-            try
-            {
-                response.Object = this.cartService.Remove(customerId, productId);
-            }
-            catch (Exception ex)
-            {
-                //log error
-                response.Errored = true;
-                response.ErrorMessage = ex.Message;
-            }
-            return response;
-        }
-
-
-        [HttpGet]
-        public Response<CheckOutResultDto> Checkout(Guid customerId)
-        {
-            Response<CheckOutResultDto> response = new Response<CheckOutResultDto>();
-            try
-            {
-                response.Object = this.cartService.CheckOut(customerId);
-            }
-            catch (Exception ex)
-            {
-                //log error
-                response.Errored = true;
-                response.ErrorMessage = ex.Message;
-            }
-            return response;
-        }
-
     }
 }
--- eCommerce/ApplicationLayer/Carts/CartService.cs
+++ eCommerce/ApplicationLayer/Carts/CartService.cs
@@ -35,8 +35,7 @@ namespace eCommerce.ApplicationLayer.Carts
             CartDto cartDto = null;
 
             Customer customer = this.customerRepository.FindById(customerId);
-            if (customer == null)
-                throw new Exception(String.Format("Customer was not found with this Id: {0}", customerId));
+            ValidateCustomer(customerId, customer);
 
             Cart cart = this.cartRepository.FindOne(new CustomerCartSpec(customerId));
             if (cart == null)
@@ -46,7 +45,7 @@ namespace eCommerce.ApplicationLayer.Carts
             }
 
             Product product = this.productRepository.FindById(productDto.ProductId);
-            this.validateProduct(product.Id, product);
+            this.ValidateProduct(product.Id, product);
 
             //Double Dispatch Pattern
             cart.Add(CartProduct.Create(customer, cart, product,
@@ -62,10 +61,10 @@ namespace eCommerce.ApplicationLayer.Carts
             CartDto cartDto = null;
 
             Cart cart = this.cartRepository.FindOne(new CustomerCartSpec(customerId));
-            this.validateCart(customerId, cart);
+            this.ValidateCart(customerId, cart);
 
             Product product = this.productRepository.FindById(productId);
-            this.validateProduct(productId, product);
+            this.ValidateProduct(productId, product);
 
             cart.Remove(product);
             cartDto = Mapper.Map<Cart, CartDto>(cart);
@@ -76,7 +75,7 @@ namespace eCommerce.ApplicationLayer.Carts
         public CartDto Get(Guid customerId)
         {
             Cart cart = this.cartRepository.FindOne(new CustomerCartSpec(customerId));
-            this.validateCart(customerId, cart);
+            this.ValidateCart(customerId, cart);
 
             return Mapper.Map<Cart, CartDto>(cart);
         }
@@ -86,7 +85,7 @@ namespace eCommerce.ApplicationLayer.Carts
             CheckOutResultDto checkOutResultDto = new CheckOutResultDto();
 
             Cart cart = this.cartRepository.FindOne(new CustomerCartSpec(customerId));
-            this.validateCart(customerId, cart);
+            this.ValidateCart(customerId, cart);
 
             Customer customer = this.customerRepository.FindById(cart.CustomerId);
 
@@ -107,16 +106,36 @@ namespace eCommerce.ApplicationLayer.Carts
             return checkOutResultDto;
         }
 
-        private void validateCart(Guid customerId, Cart cart)
+        public CartDto Share(Guid cartOwnerId, Guid cartRecipientId)
+        {
+            var cart = this.cartRepository.FindOne(new CustomerCartSpec(cartOwnerId));
+            this.ValidateCart(cartOwnerId, cart);
+
+            var recipient = this.customerRepository.FindById(cartRecipientId);
+            ValidateCustomer(cartRecipientId, recipient);
+
+            var sharedCart = cart.Share(recipient);
+            this.cartRepository.Add(sharedCart);
+
+            return Mapper.Map<Cart, CartDto>(sharedCart);
+        }
+
+        private void ValidateCart(Guid customerId, Cart cart)
         {
             if (cart == null)
                 throw new Exception(String.Format("Customer was not found with this Id: {0}", customerId));
         }
 
-        private void validateProduct(Guid productId, Product product)
+        private void ValidateProduct(Guid productId, Product product)
         {
             if (product == null)
                 throw new Exception(String.Format("Product was not found with this Id: {0}", productId));
+        }
+
+        private void ValidateCustomer(Guid customerId, Customer customer)
+        {
+            if (customer == null)
+                throw new ArgumentNullException($"Customer was not found with this Id: {customerId}");
         }
     }
 }
